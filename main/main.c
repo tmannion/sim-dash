fa6e488a98cc7be2b9c985f46033fb84c1d89797
Tremainm <147380462+Tremainm@users.noreply.github.com>
2026-06-11 19:52:55 +0100

    led_strip testing working as expected

--- main/main.c
+++ main/main.c
@@ -1,20 +1,31 @@
-/*
- * sim-dash
- *
- * F1 Game sim dashboard 
- *
- * 05-2026
- */
-
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
 #include "led_strip.h"
-#include "udp_receiver.h"
-#include "f1_telemetry.h"
 
 void app_main(void)
 {
+    led_strip_init();
+    led_strip_clear();
 
+    while (1) {
+        // Red for 1 second
+        led_strip_set_pixel(0, 255, 0, 0);
+        led_strip_refresh();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        // Green for 1 second
+        led_strip_set_pixel(0, 0, 255, 0);
+        led_strip_refresh();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        // Blue for 1 second
+        led_strip_set_pixel(0, 0, 0, 255);
+        led_strip_refresh();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        // Off for half a second
+        led_strip_clear();
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 }
